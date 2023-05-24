8c61290be9675cabde747c8bf59a91ff0065f433
gurkansatr <gurkan-satir@hotmail.com>
2023-05-24 16:42:36 +0300

    s5g2cevaplar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
@@ -7,3 +8,49 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+const titles =document.querySelectorAll("h2");
+const images = document.querySelectorAll("img")
+images.forEach(resim => {
+    resim.addEventListener("mouseover",(event)=>{
+        event.target.style.filter = "grayscale(100%)";
+    })
+    resim.addEventListener("mouseout",(event)=>{
+        event.target.style.filter = "grayscale(0)";
+    })
+})
+
+window.addEventListener("load", (event)=>{
+setTimeout(() => {
+    document.querySelector("body").style.background = "red"
+}, "2000");
+})
+images.forEach(resim =>{
+    resim.addEventListener("click",(event)=>{
+        event.target.style.width="50%"
+    })
+    resim.addEventListener("dblclick",(event)=>{
+        event.target.style.width="100%"
+    })
+})
+window.addEventListener("keypress" , (event)=> {
+    document.querySelectorAll()
+})
+
+window.addEventListener("resize" , (e) =>{
+    if(window.outerWidth < 600){
+        body.style.backgroundColor = "blue"
+    }
+})
+
+titles.forEach(title => {
+    let starting = 0;
+    title.addEventListener("mouseover", () =>{
+        title.style.letterSpacing = starting++ + "px";
+    });
+    title.addEventListener("mouseover", () =>{
+        title.style.fontWeight = "bold";
+        title.style.textTransform = "uppercase";
+        title.style.color = `rgb(${Math.random() * 255},
+        ${Math.random() * 255},${Math.random() * 255})`
+    });
+});
